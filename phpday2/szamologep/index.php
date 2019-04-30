615ede8c11b1942729b245f3253d6f88a075d296
domjanb <domjan.balazs@outlook.com>
2019-04-30 11:46:35 +0200

    mara vege

---
+++ phpday2/szamologep/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        
+        <meta charset="UTF-8">
+        <title>Próba</title>
+    </head>
+    <body>
+        áőúéü9öúűÖÜÓÚŐŰÁÍ
+        <form action="szamol.php" method="GET">
+            A=<input type="text" name="a" id="a"/><br>
+            B=<input type="text" name="b" id="b"/><br>
+            <input type="submit" name="muvelet" value="+" />
+            <input type="submit" name="muvelet" value="-" />
+            <input type="submit" name="muvelet" value="*" />
+            <input type="submit" name="muvelet" value="/" />
+        </form>
+        <?php
+        ?>
+    </body>
+</html>
